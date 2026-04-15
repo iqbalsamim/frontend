e39a893c4d63815f527f0438bd9a69aac2267499
iqbal samim <68268842+iqbalsamim@users.noreply.github.com>
2026-04-15 13:54:39 +0430

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://192.168.80.14:5000/api"
+  baseURL: "https://your-backend.onrender.com/api"
 });
 
 // Attach token automatically
